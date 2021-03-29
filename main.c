6d6b8e7324342e22715ee936e5504595e721edad
kpdiedrich <diedrich.keith1@gmail.com>
2021-03-29 14:18:54 -0400

    Update main.c
    Implementing "worst fit" function.

--- main.c
+++ main.c
@@ -167,6 +167,29 @@ int* best_fit(int size_request, int thread_num)
 	return starting_ptr;	
 }
 
+int* worst_fit(int size_request, int thread_num)
+{
+	int i, largest = -1, mem_index;
+	int* starting_ptr = NULL;
+	
+	printf("Worst Fit\n\n");
+	for(i = 0; i < count; i++) {
+		if(memory_table[i][4] != 1 && memory_table[i][1] >= size_request) {
+			if(memory_table[i][1] > largest) {
+				largest = memory_table[i][1];
+				mem_index = i;
+				starting_ptr = memory_block_ptr;
+			}
+		}
+	}
+
+	if(largest == -1) {
+		return NULL;
+	}
+	insert_memory_block(mem_index, size_request, thread_num);
+	return starting_ptr;
+}
+
 int* memory_allocate(int size, int num) 
 {
 	int* starting_ptr;
@@ -187,11 +210,13 @@ int* memory_allocate(int size, int num)
 		return starting_ptr;		
 	}
 
-/*
 	else {
-		// worst_fit
+		starting_ptr = worst_fit(size, num);
+		if(starting_ptr != NULL) {
+			curr_mem_size += size;
+		}
+		return starting_ptr;
 	}
-*/
 }
 
 void memory_deallocate(int size, int num)
@@ -302,12 +327,12 @@ void *user(void *arg)       // Starting function for user threads
 	while(!user_thread->wake_up && user_thread->sleep_time != 0) {
 		user_thread->sleep_time--;
 	}
-	sleep(1);
 	pthread_mutex_lock(&print);
 	printf("Thread %d waking up\n", user_thread->num_thread);
 	pthread_mutex_unlock(&print);
 	sem_wait(&buff2);		// Decrement buff2
 	pthread_mutex_lock(&mutx2);
+	sleep(1);
 	while(user_th_buff2[position2 % BUFFER_SIZE] != NULL) {		// Search for open element in buffer
 		position2++;
 	}
@@ -323,8 +348,8 @@ int main(int argc, char **argv)
 	THREAD_NUM = atoi(argv[1]);
 	FUNCTION_NUM = atoi(argv[2]);
 
-	//THREAD_NUM = 16;
-	//FUNCTION_NUM = 2;
+	//THREAD_NUM = 25;
+	//FUNCTION_NUM = 3;
 	//defrag = atoi(argv[3]);
 	int i;
 	memory_block_ptr = malloc(MAX_MEMORY_SIZE * sizeof(int));		// Allocate block of memory
@@ -343,7 +368,7 @@ int main(int argc, char **argv)
 	state1 = pthread_mutex_init(&mutx, NULL);
 	state4 = pthread_mutex_init(&mutx2, NULL);
 	state6 = pthread_mutex_init(&print, NULL);
-    state2 = sem_init(&semEmpty, 0, BUFFER_SIZE / 2);
+	state2 = sem_init(&semEmpty, 0, BUFFER_SIZE / 2);
 	state5 = sem_init(&buff2, 0, BUFFER_SIZE);
 	//mutex initialization
 	//semaphore initialization, first value = 0
@@ -364,22 +389,22 @@ int main(int argc, char **argv)
 
 	// Create MMS Thread and THREAD_NUM User Threads
 	if(pthread_create(&mms_th, NULL, mms, &mms_thread) != 0) {
-        perror("Failed to create MMS thread");   
+		perror("Failed to create MMS thread");   
 	}
 	for(i = 0; i < THREAD_NUM; i++) {
 		if(pthread_create(user_th + i, NULL, user, &thread_args[i]) != 0) {
-            perror("Failed to create user thread");
-        }  	
+			perror("Failed to create user thread");
+		}	  	
 	}
 
 	// Waiting thread to terminate
 	if(pthread_join(mms_th, NULL) != 0) {
-        perror("Failed to join MMS thread");
-    }
+		perror("Failed to join MMS thread");
+	}
 	for(i = 0; i < THREAD_NUM; i++) {
 		if(pthread_join(user_th[i], NULL) != 0) {
-            perror("Failed to join user thread");
-        }
+			perror("Failed to join user thread");
+		}
 	}
 
 	sem_destroy(&semEmpty);         // Destroy semaphore
